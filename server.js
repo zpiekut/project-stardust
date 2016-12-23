6b9b674f154104884a4811ead27911c48e7a93da
Zach Piekut <zpiekut1@gmail.com>
2016-12-22 20:22:07 -0500

    add swagger documentation

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 'use strict';
 
-const Hapi      = require('hapi');
-const env       = process.env.NODE_ENV || 'development';
-const config    = require(__dirname + '/config/config')[env];
-const models    = require('./models');
-const server = new Hapi.Server();
+const Hapi        = require('hapi');
+const Inert       = require('inert');
+const Vision      = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const env         = process.env.NODE_ENV || 'development';
+const config      = require(__dirname + '/config/config')[env];
+const models      = require('./models');
+const Pack        = require('./package');
+const server      = new Hapi.Server();
+
+const options = {
+  info: {
+    'title': 'Test API Documentation',
+    'version': Pack.version,
+  }
+};
 
 server.connection({ port: 3000 });
 server.route(require('./lib/routes/user'));
@@ -13,6 +24,11 @@ server.route(require('./lib/routes/credit'));
 server.route(require('./lib/routes/redemption'));
 server.route(require('./lib/routes/work-session'));
 server.route(require('./lib/routes/credit-transaction'));
+
+server.register([ Inert, Vision, {
+  'register': HapiSwagger,
+  'options': options
+}]);
 
 models.sequelize.sync().then(function() {
   server.start(function() {
