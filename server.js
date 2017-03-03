bbe517619a36b0a418d83c196dea9297183a1a94
Neel Krishna <neelkri93@gmail.com>
2017-03-02 21:15:26 -0500

    framework

--- server.js
+++ server.js
@@ -25,6 +25,56 @@ server.route(require('./lib/routes/redemption'));
 server.route(require('./lib/routes/work-session'));
 server.route(require('./lib/routes/credit-transaction'));
 
+var users = { // collect users from db
+  1: {
+    id: 1,
+    name: 'Jen Jones'
+  }
+};
+
+//fill this out
+var validate = function (decoded, request, callback) {
+
+  // do your checks to see if the person is valid
+  if (!people[decoded.id]) {
+    return callback(null, false);
+  }
+  else {
+    return callback(null, true);
+  }
+};
+
+server.register(require('hapi-auth-jwt2'), function (err) {
+
+  if(err){
+    console.log(err);
+  }
+
+  server.auth.strategy('jwt', 'jwt',
+      { key: 'NeverShareYourSecret',          // Never Share your secret key
+        validateFunc: validate,            // validate function defined above
+        verifyOptions: { algorithms: [ 'HS256' ] } // pick a strong algorithm
+      });
+
+  server.auth.default('jwt');
+
+  server.route([
+    {
+      method: "GET", path: "/", config: { auth: false },
+      handler: function(request, reply) {
+        reply({text: 'Token not required'});
+      }
+    },
+    {
+      method: 'GET', path: '/restricted', config: { auth: 'jwt' },
+      handler: function(request, reply) {
+        reply({text: 'You used a Token!'})
+            .header("Authorization", request.headers.authorization);
+      }
+    }
+  ]);
+});
+
 server.register([ Inert, Vision, {
   'register': HapiSwagger,
   'options': options
