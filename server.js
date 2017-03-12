897bdcc78c525d4d6ac64a730e48fbe258500b9b
Neel Krishna <neelkri93@gmail.com>
2017-03-12 15:26:50 -0400

    fully integrated authentication -- validate method bug. Note: Server running at 3000 on this branch

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const Hapi      = require('hapi');
 const env       = process.env.NODE_ENV || 'development';
 const config    = require(__dirname + '/config/config')[env];
 const models    = require('./models');
-var passport	= require('passport');
+const uuid      = require('uuid');
+const nJwt      = require('njwt');
 const server = new Hapi.Server();
 
 server.connection({ port: 3000 });
@@ -14,22 +15,15 @@ server.route(require('./lib/routes/credit'));
 server.route(require('./lib/routes/redemption'));
 server.route(require('./lib/routes/work-session'));
 server.route(require('./lib/routes/credit-transaction'));
-server.route(require('./lib/routes/login'));
-server.route(require('./lib/routes/logout'));
+server.route(require('./lib/routes/auth'));
 
-
-var users = { // collect users from db
-  1: {
-    id: 1,
-    name: 'Jen Jones'
-  }
-};
 
 //fill this out
 var validate = function (decoded, request, callback) {
-
-  // do your checks to see if the person is valid
-  if (!people[decoded.id]) {
+  var secretKey = uuid.v4();
+  var token = request.headers.tokenPassed;
+  var verifiedJwt = nJwt.verify(token,secretKey);
+  if (!verifiedJwt) {
     return callback(null, false);
   }
   else {
@@ -50,22 +44,6 @@ server.register(require('hapi-auth-jwt2'), function (err) {
       });
 
   server.auth.default('jwt');
-
-  server.route([
-    {
-      method: "GET", path: "/", config: { auth: false },
-      handler: function(request, reply) {
-        reply({text: 'Token not required'});
-      }
-    },
-    {
-      method: 'GET', path: '/restricted', config: { auth: 'jwt' },
-      handler: function(request, reply) {
-        reply({text: 'You used a Token!'})
-            .header("Authorization", request.headers.authorization);
-      }
-    }
-  ]);
 });
 
 models.sequelize.sync().then(function() {
